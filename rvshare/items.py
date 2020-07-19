6998cafa84b23428a78cff8bc14f18973a658e7f
brandoncryu <chryu1@gmail.com>
2020-07-19 01:57:37 -0500

    scraping total 16 features

--- rvshare/items.py
+++ rvshare/items.py
@@ -10,13 +10,21 @@ import scrapy
 
 class RvshareItem(scrapy.Item):
     name = scrapy.Field()
-    price = scrapy.Field()
+    price_nightly = scrapy.Field()
+    price_weekly = scrapy.Field()
+    price_monthly = scrapy.Field()
     location = scrapy.Field()
     sleeps = scrapy.Field()
     year = scrapy.Field()
     vehicle_type = scrapy.Field()
     length = scrapy.Field()
     distance = scrapy.Field()
+    rv_details = scrapy.Field()
+    kitchen = scrapy.Field()
+    bathroom =  scrapy.Field()
+    temperature_control = scrapy.Field()
+    entertainment = scrapy.Field()
+    cancellation = scrapy.Field()
 
     
 
--- rvshare/spiders/rvshare_spider.py
+++ rvshare/spiders/rvshare_spider.py
@@ -9,34 +9,36 @@ class RvshareSpider(Spider):
 
     def parse(self, response):
         city_urls = response.xpath('//ul[@class="popular-destinations__list"]')[1].xpath('./li[@class="popular-destinations__list-item"]/a/@href').extract()[:-1]
-        print(city_urls)
-        for url in city_urls[:2]:
+        # print(city_urls)
+        for url in city_urls[:5]:
             yield Request(url=url, callback=self.parse_city_listings)
 
     def parse_city_listings(self, response):
         num_pages = response.xpath('//div[@class="Box-mhcuhk-0 Text-pe2fh5-0 ghgJjM"]/text()').extract()
         num_pages = int(int(re.findall('of (\d+)',num_pages[0])[0])/27)
         initial_url = response.request.url
-        print('='*50)
-        print(num_pages)
+        # print('='*50)
+        # print(num_pages)
 
-        url_list = [f'{initial_url}?page={i}' for i in range(num_pages)]
-        for url in url_list[:2]:
+        url_list = [f'{initial_url}?page={i}' for i in range(1, num_pages+1)]
+        for url in url_list[:5]:
             yield Request(url=url, callback=self.parse_results_page)
 
     def parse_results_page(self, response):
         listing_urls = response.xpath('//div[@class="Box-mhcuhk-0 Flex-ofb2h0-0 RVCardstyles__MetaWrapper-sc-1bwuzqm-5 hCxFSr"]/a/@href').extract()
         listing_urls = [f'https://rvshare.com/{suffix}' for suffix in listing_urls]
         distance_list = response.xpath('//div[@class="Box-mhcuhk-0 Flex-ofb2h0-0 lfwVrU"]/div/span/span/text()').extract()
-        print(distance_list)
+        # print(distance_list)
         
-        for i, url in enumerate(listing_urls[:2]):
+        for i, url in enumerate(listing_urls[:5]):
             yield Request(url=url, callback=self.parse_listing_page, meta={'distance': distance_list[i][:]})
 
     def parse_listing_page(self, response):
         name = response.xpath('//div[@class="RvTitle__RvTitleContainer-hskm81-0 ecAmPT"]/div/h1/text()').extract_first()
-        price = response.xpath('//div[@data-id="nightly-rate"]/span/text()').extract_first()
-        
+        # price = response.xpath('//div[@data-id="nightly-rate"]/span/text()').extract_first()
+        price_nightly = response.xpath('//*[@id="rates"]/div[2]/div/div[1]/div[2]/div[1]/div/div[1]/text()').extract_first()
+        price_weekly = response.xpath('//*[@id="rates"]/div[2]/div/div[1]/div[2]/div[2]/div/div[1]/text()').extract_first()
+        price_monthly = response.xpath('//*[@id="rates"]/div[2]/div/div[1]/div[2]/div[3]/div/div[1]/text()').extract_first()
         summary = response.xpath('//div[@class="styles__SummaryItem-sc-1ylo37q-1 ddrTmu"]/text()').extract()
         
         location = summary[0]
@@ -47,15 +49,32 @@ class RvshareSpider(Spider):
 
         distance = response.meta['distance']
 
+        rv_details = response.xpath('//div[@data-id="rv-details"]/ul/li/span/text()').extract()
+        kitchen = response.xpath('//div[@data-id="kitchen-amenities"]/ul/li/span/text()').extract()
+        bathroom = response.xpath('//div[@data-id="bathroom-amenities"]/ul/li/span/text()').extract()
+        temperature_control = response.xpath('//div[@data-id="temperature-amenities"]/ul/li/span/text()').extract()
+        entertainment = response.xpath('//div[@data-id="entertainment-amenities"]/ul/li/span/text()').extract()
+        cancellation = response.xpath('//div[@class="CancellationPolicy__Container-sc-1vbh5ca-0 jSIiWW"]/div/text()').extract_first()
+        cancellation = re.findall('- (\D+)', cancellation)[0]
+
         item = RvshareItem()
         item['name'] = name
-        item['price'] = price
+        item['price_nightly'] = price_nightly
+        item['price_weekly'] = price_weekly
+        item['price_monthly'] = price_monthly
         item['location'] = location
         item['sleeps'] = sleeps
         item['year'] = year
         item['vehicle_type'] = vehicle_type
         item['length'] = length
         item['distance'] = distance
+        item['rv_details'] = rv_details
+        item['kitchen'] = kitchen
+        item['bathroom'] = bathroom
+        item['temperature_control'] = temperature_control
+        item['entertainment'] = entertainment
+        item['cancellation'] = cancellation
+        
 
         yield item
         
